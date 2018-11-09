13c8a4919fd64eddd6d95718a5224b63eed9f300
ymsk.sky <ymsk.sky.95@gmail.com>
2018-11-09 18:14:28 +0900

    add, edit calculateCRC16

--- calculateCRC16.py
+++ calculateCRC16.py
@@ -1,8 +1,40 @@
 # -*- coding: utf-8 -*-
 
-def makeCRC16():
-    pass
+# 文字列(str)へ一度変換してからCRC-16を計算する
 
+def addCRC16(cmd):
+    # CRC16でエラーチェック値を計算する
+    crc = makeCRC16(cmd)
+    # エラーチェック値をクエリの末尾に追加する
+    # 上位と下位を入れ替える
+    crc = transposeHigherLower(crc)
+    # エラーチェック値は下位→上位の順
+    cmd = cmd + crc
+    return cmd
+
+def makeCRC16(cmd):
+    # 1バイト毎のリストを作成
+    byte_list = toListOfByte(cmd)
+    # CRC-16の計算
+    for b in byte_list:
+        crc = b"\xb5\xd2"
+    return crc
+
+def transposeHigherLower(crc):
+    tmp = crc.decode()
+    tmp = tmp[1] + tmp[0]
+    crc = tmp.encode()
+    return crc
+
+def toListOfByte(cmd):
+    # 文字列へ変換
+    cmd_hex = cmd.hex()
+    # 2文字(1バイト)毎のリストに変換
+    byte_list = [cmd_hex[i: i+2] for i in range(0, len(cmd_hex), 2)]
+    return byte_list
 
 if __name__ == '__main__':
-    pass
+    commando = b"\x01\x03\x00\x7f\x00\x01"
+    ans = b"\xb5\xd2"
+    commando = addCRC16(commando)
+    print(commando)
--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
-response = b"\x01\x03\x00\x7f\x00\x01\xb5\xd2"
+# response = b"\x01\x03\x00\x7f\x00\x01\xb5\xd2"
 
-print(type(response))
-print(response.hex())
+crc = b"\x66"
+
+print(crc)
+
+tmp = crc.decode()
+print(tmp)
+crc = tmp.encode()
+
+print(crc)
+print(type(crc))
