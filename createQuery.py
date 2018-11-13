6d9a0f9f8c53191b73368668da03b66f470af5d0
ymsk.sky <ymsk.sky.95@gmail.com>
2018-11-13 17:48:34 +0900

    complete createQuery

--- createQuery.py
+++ createQuery.py
@@ -18,19 +18,46 @@ def createSlaveAddress():
 def createFunctionCode(f):
     return functionCodeData[f]
 
-def createData(fc):
-    if(fc == READ_REGISTER) {
+def createData(fc, me=0, pos=0, sp=0, strt=0, stp=0):
+    if(fc == functionCodeData[READ_REGISTER]):
         ### リモートI/Oアクセス
         # レジスタアドレス007f, レジスタ数0001
         return b"\x00\x7f\x00\x01"
-    }elif(fc == WRITE_REGISTERS) {
+    elif(fc == functionCodeData[WRITE_REGISTERS]):
         ### ダイレクトデータ運転
-    }
+        # 書き込みレジスタ先頭アドレス
+        head_register_adress = b"\x00\x58"
+        # 書き込みレジスタ数
+        register_number = b"\x00\x10"
+        # 書き込みバイト数
+        byte_number = b"\x20"
+        # 運転データNo.
+        operation_data_no = b"\x00\x00\x00\x00"
+        # 方式
+        method = me.to_bytes(4, "big")
+        # 位置
+        position = pos.to_bytes(4, "big")
+        # 速度
+        speed = sp.to_bytes(4, "big")
+        # 起動・変速レート
+        start_shift_rate = strt.to_bytes(4, "big")
+        # 停止レート
+        stop_rate = stp.to_bytes(4, "big")
+        # 運転電流
+        operating_current = b"\x00\x00\x03\xe8"
+        # 反映トリガ
+        reflect_trigger = b"\x00\x00\x00\x01"
+        # データを作成
+        result = head_register_adress + register_number + byte_number + \
+                    operation_data_no + method + position + speed + \
+                    start_shift_rate + stop_rate + operating_current + \
+                    reflect_trigger
+        return result
 
 def test():
     sa = createSlaveAddress()
-    fc = createFunctionCode(READ_REGISTER)
-    d = createData(READ_REGISTER)
+    fc = createFunctionCode(WRITE_REGISTERS)
+    d = createData(fc, me=2, pos=8500, sp=2000, strt=1500, stp=1500)
     query_without_crc = sa + fc + d
     # 結果を表示
     print(query_without_crc)
