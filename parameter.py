49a2e2da1ec434ed233a8d992cf92c88e99a3020
ymsk.sky <ymsk.sky.95@gmail.com>
2018-11-19 12:04:13 +0900

    add comment on parameter.py

--- parameter.py
+++ parameter.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
-##### 方式一覧
+##### ダイレクトデータ運転 運転データNo.
+DATA_NO_DEFAULT = 0
+DATA_NO_MIN = 0
+DATA_NO_MAX = 255
+
+##### ダイレクトデータ運転 方式一覧
+### 値: from 0 to 22 except 4,5,6,19
 # 設定なし
 NO_SETTING = 0
 # 絶対位置決め
@@ -39,3 +45,56 @@ ABUSOLUTE_POSITION_PRESS = 20
 RELATIVE_POSITION_WITH_ORDER_POSITION = 21
 # 相対位置決め押し当て（検出位置基準）
 RELATIVE_POSITION_WITH_DETECT_POSITION = 22
+
+##### ダイレクトデータ運転 位置
+# [step]
+POSITION_DEFAULT = 0
+POSITION_MIN = -2147483648
+POSITION_MAX = 2147483648
+
+##### ダイレクトデータ運転 速度
+# [Hz]
+SPEED_DEFAULT = 1000
+SPEED_MIN = -4000000
+SPEED_MAX = 4000000
+
+##### ダイレクトデータ運転 起動・変速レート
+# 1 = 0.001[kHz/s], 0.001[s], 0.001[ms/kHz]
+START_SHIFT_RATE_DEFAULT = 1000000
+START_SHIFT_RATE_MIN = 1
+START_SHITF_RATE_MAX = 1000000000
+
+##### ダイレクトデータ運転 停止レート
+# 1 = 0.001[kHz/s], 0.001[s], 0.001[ms/kHz]
+STOP_RATE_DEFAULT = 1000000
+STOP_RATE_MIN = 1
+STOP_RATE_MAX = 1000000000
+
+##### ダイレクトデータ運転 運転電流
+# 1 = 0.1[%]
+CURRENT_DEFAULT = 1000
+CURRENT_MIN = 0
+CURRENT_MAX = 1000
+
+##### ダイレクトデータ運転 反映トリガ
+# -7: 運転データNo.
+# -6: 方式
+# -5: 位置
+# -4: 速度
+# -3: 起動・変速レート
+# -2: 停止レート
+# -1: 運転電流
+# 0: 無効
+# 1: 全データ反映
+### マイナス値はF埋めする
+### 例）-1→FFFF FFFF, -2→FFFF FFFE
+TRIGGER_DEFAULT = 0
+TRIGGER_MIN = -7
+TRIGGER_MAX = 1
+
+##### ダイレクトデータ運転 転送先
+# 0: 実行メモリ
+# 1: バッファメモリ
+TRANSFER_DEFAULT = 0
+TRANSFER_MIN = 0
+TRANSFER_MAX = 1
--- test.py
+++ test.py
@@ -1,92 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import os
-import serial
 import time
-# import threading
 import concurrent.futures
 
-client = serial.Serial()
-size = 16
-
-def test():
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
-    executor.submit(func1)
-    executor.submit(func2)
-    executor.submit(func3)
+a = 10
 
 def func1():
-    for _ in range(100):
-        print("func1")
-    print("func1 is finished")
+    global a
+    while(True):
+        print(a)
+        time.sleep(0.1)
 
 def func2():
-    for _ in range(100):
-        print("func2")
-    print("func2 is finished")
-
-def func3():
-    for _ in range(100):
-        print("func3")
-    print("func3 is finished")
-
-def io_test():
-    set_serial()
-    client.open()
-    ##### direct data operation #####
-    for _ in range(1):
-        # direct = b"\x01\x10\x00\x58\x00\x10\x20\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x21\x34\x00\x00\x07\xd0\x00\x00\x05\xdc\x00\x00\x05\xdc\x00\x00\x03\xe8\x00\x00\x00\x01\x1c\x08"
-        # client.write(direct)
-        # standby()
-        # response = client.read(size)
-        # print(response)
-        # standby()
-        ##### remote I/O accessing #####
-        command = b"\x01\x03\x00\x7f\x00\x01\xb5\xd2"
-        while(True):
-            client.write(command)
-            standby()
-            response = client.read(size)
-            move = print_move_status(response)
-            print("{0:016d}".format(int(bin(((response[3] << 8) + response[4]))[2:])))
-            standby()
-            if(move == 1):
-                break
-        #####
-    client.close()
-
-def print_move_status(response):
-    driver_output = (response[3] << 8) + response[4]
-    move = (driver_output >> 13) & 1
-    if(move == 1):
-        pass
-        #print("Moving")
-    elif(move == 0):
-        pass
-        #print("NOT Moving: stopping")
-    return move
-
-def standby():
-    time.sleep(0.02)
-
-def set_serial():
-    client.port = get_com_port()
-    client.baudrate = 115200
-    client.bytesize = 8
-    client.timeout = 0.01
-    client.parity = serial.PARITY_EVEN
-    client.stopbits = serial.STOPBITS_ONE
-
-def get_com_port():
-    for file in os.listdir('/dev'):
-        if "tty.usbserial" in file:
-            return '/dev/' + file
-    return
+    global a
+    while(True):
+        a += 1
+        print("func2: increment a")
+        time.sleep(1)
 
 def main():
-    pass
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+    executor.submit(func1)
+    executor.submit(func2)
+
+def test():
+    val = 2147483648
+    val2 = 2147483648
+    print(val)
+    print(type(val))
+    print(val2)
+    print(type(val2))
 
 if __name__ == "__main__":
     # main()
-    # io_test()
     test()
