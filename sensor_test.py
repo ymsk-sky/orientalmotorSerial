6eefbdc29b1bd83443246183999a2643aca5f855
ymsk.sky <ymsk.sky.95@gmail.com>
2018-12-10 17:14:15 +0900

    test serial communication to send and receive packets on sensor_test.py

--- sensor_test.py
+++ sensor_test.py
@@ -1,71 +1,38 @@
 # -*- coding: utf-8 -*-
 
-class DummySerial():
-    __SERIAL_FG = False
-    __serial = {"port": "/dev/comport", "baudrate": 0, "bytesize": 8}
+import serial
+import time
 
-    def __init__(self):
-        pass
+def set_serial(client):
+    client.port = "/dev/tty.usbmodem1421"
+    client.baudrate = 19200
+    client.parity = 'N'
+    client.stopbits = serial.STOPBITS_ONE
+    client.timeout = 0.1
 
-    def set_serial(self, p="", b=9600, s=8):
-        self.__serial["port"] = p
-        self.__serial["baudrate"] = b
-        self.__serial["bytesize"] = s
-
-    def read_serial(self):
-        if(self.__SERIAL_FG):
-            pass
-        else:
-            print("NOT OPEN SERIAL")
-
-    def write_serial(self, cmd):
-        if(self.__SERIAL_FG):
-            pass
-        else:
-            print("NOT OPEN SERIAL")
-
-    def open_serial(self):
-        if(self.__SERIAL_FG):
-            print("ALREADY OPENED SERIAL")
-            exit()
-        else:
-            self.__SERIAL_FG = True
-
-    def close_serial(self):
-        if(self.__SERIAL_FG):
-            self.__SERIAL_FG = False
-        else:
-            print("NOT OPEN SERIAL")
-
-    def print_serial(self):
-        print(self.__serial)
-
-class DriverSerial(DummySerial):
-    def __init__(self):
-        pass
-
-class SensorSerial(DummySerial):
-    def __init__(self):
-        pass
-
-class Main():
-    def main(self):
-        driver = DriverSerial()
-        sensor = SensorSerial()
-        driver.open_serial()
-        sensor.open_serial()
-        ##### 処理 - 開始 #####
-        # 初期設定
-        driver.set_serial(p="usbserial/driver_port", b=115200, s=8)
-        sensor.set_serial(p="usbserial/sensor_port", b=19200, s=8)
-        # ループ
-        while(True):
-            break
-        ##### 処理 - 終了 #####
-        driver.close_serial()
-        sensor.close_serial()
-
+def main():
+    client = serial.Serial()
+    set_serial(client)
+    client.open()
+    # print(client)
+    print("# OPEN AND SLEEP 2 SECONDS")
+    time.sleep(2)   # waiting for reseting Arduino
+    print("# START LOOP")
+    # -- start serial communication --
+    for _ in range(3):
+        h = 255
+        r = 1
+        g = 55
+        b = 254
+        # v = (h.to_bytes(1, "big") + r.to_bytes(1, "big")
+        #      + g.to_bytes(1, "big") + b.to_bytes(1, "big"))
+        # client.write(v)
+        time.sleep(1)
+        res = client.read(size=16)
+        print(res)
+        time.sleep(1)
+    # -- -- -- -- -- -- -- -- -- -- --
+    client.close()
 
 if __name__ == "__main__":
-    main = Main()
-    main.main()
+    main()
