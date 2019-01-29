1d1853e00e6b0b0014ddee5f9aba867b6559a3a3
ymsk.sky <ymsk.sky.95@gmail.com>
2019-01-29 17:55:18 +0900

    make base templete in robot_control.py

--- robot_control.py
+++ robot_control.py
@@ -4,11 +4,22 @@ import os
 import serial
 from time import sleep
 
+# マイコンのポートを取得する
+def get_port_micro():
+    for file in os.listdir('/dev'):
+        if 'tty.usbmodem' in file:
+            return '/dev/' + file
+
 def main():
     driver = serial.Serial('/dev/tty.usbserial-FT1GOG9N', 115200,
                            parity='E', timeout=0.01)
-    micro = serial.Serial('/dev/tty.usbmodem1421', 19200)
+    micro = serial.Serial(get_port_micro(), 19200)
+    sleep(2)    # Arduino初期化を待機
+    # ドライバ状態確認
 
+    while(True):
+        break
+    # 終了
     driver.close()
     micro.close()
 
