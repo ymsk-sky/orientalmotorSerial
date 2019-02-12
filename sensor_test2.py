019f8d0cbf487dd0aef400c6627330b82a65241e
ymsk.sky <ymsk.sky.95@gmail.com>
2019-02-12 17:02:21 +0900

    add check checksum on sensor_test2.py

--- sensor_test2.py
+++ sensor_test2.py
@@ -3,16 +3,27 @@
 import serial
 import time
 
+SENSOR_NUM = 1
+
 def print_sensor_value(response):
     # r = b"\xFF\x11\x22\xEE"
     # ヘッダー, センサ値(上位), センサ値(下位), チェックサム
     # センサ値は1ビット左シフトしているため元に戻す必要がある
     # マイナス値の場合桁あふれでエラーになりそう
-    SENSOR_NUM = 1
+
+    # ヘッダ確認
     head = response[0]
     if(head != 0xFF):
         # ヘッダが異なる
         return
+    # チェックサム確認
+    checksum = 0
+    for x in range(SENSOR_NUM):
+        checksum += response[1+2*x] + response[2+2*x]
+    if(checksum != response[-1]):
+        # チェックサムが異なる
+        return
+    # 処理
     for i in range(SENSOR_NUM):
         higher = response[1+2*i]   # 1, 3, 5, 7, ...
         lower = response[2+2*i]    # 2, 4, 6, 8, ...
@@ -30,9 +41,9 @@ def main():
         pass
     print("### ARDUINO IS READY ###")
     while(True):
-        ser.write("\xFF")
+        ser.write("\x00")
         time.sleep(0.1)
-        response = ser.read(16)
+        response = ser.read(2+SENSOR_NUM)
         print_sensor_value(response)
         time.sleep(0.5)
     ser.close()
