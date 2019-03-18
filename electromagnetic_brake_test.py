f5c40702cc775af13b7775df1f08eeee25ae9e1a
ymsk.sky <ymsk.sky.95@gmail.com>
2019-03-18 17:36:41 +0900

    remake electromagnetic_brake_test.py to measure waiting time from on to off

--- electromagnetic_brake_test.py
+++ electromagnetic_brake_test.py
@@ -1,133 +1,107 @@
 # -*- coding: utf-8 -*-
 
-import os
 import serial
 import time
 
-def get_port():
-    for file in os.listdir('/dev'):
-        if 'tty.usbserial' in file:
-            return '/dev/' + file
+# ループ回数
+NUM = 2
+# 電磁ブレーキ待機時間
+WAIT_ON = 0.15
+WAIT_OFF = 0.05
 
-def set_serial(c):
-    c.port = get_port()
-    c.baudrate = 115200
-    c.bytesize = 8
-    c.parity = 'E'
-    c.stopbits = 1
-    c.timeout = 0.01
+# 回転が停止状態かを確認
+def stop_rotation(ser, response):
+    if(not (len(response) == 7)):
+        ser.reset_input_buffer()
+        return False
+    status = (response[3] << 8) + response[4]
+    move = (status >> 13) & 1
+    if(move):
+        return False
+    else:
+        return True
 
-def mbc_status(r):
-    return (((r[3] << 8) + r[4]) >> 9) & 1
-
-def move_status(r):
-    return (((r[3] << 8) + r[4]) >> 13) & 1
+# CRC-16/Modbusによるエラーチェック
+def crc_error_check(query):
+    crc_register = 0xFFFF
+    for data_byte in query:
+        crc_register ^= data_byte
+        for _ in range(8):
+            overflow = crc_register & 1 == 1
+            crc_register >>= 1
+            if overflow:
+                crc_register ^= 0xA001
+    return crc_register.to_bytes(2, 'little')
 
 def main():
-    print("##### START")
-    client = serial.Serial()
-    set_serial(client)
-    client.open()
-    print("##### SERIAL OPEN")
-    # -------- 開始 --------
-    # クエリ作成
-    ## MBC(電磁ブレーキの状態)読み出し
-    q_mbc = b"\x01\x03\x01\x7b\x00\x01\xf5\xef"
-    ## C-ONをON = 電磁ブレーキを解放
+    ser = serial.Serial()
+    ser.port = '/dev/tty.usbserial-FT1GOG9N'
+    ser.baudrate = 115200
+    ser.parity = serial.PARITY_EVEN
+    ser.timeout = 0.01
+    ser.open()
+    # ---- ---- ---- ----
+    # C-ONをON(電磁ブレーキを解放)
     q_release = b"\x01\x06\x00\x7d\x00\x04\x18\x11"
-    ## C-ONをOFF = 電磁ブレーキを保持
+    # C-ONをOFF(電磁ブレーキを保持)
     q_keep = b"\x01\x06\x00\x7d\x00\x00\x19\xd2"
-    ## ダイレクトデータ運転（例）
-    q_direct = (b"\x01\x10\x00\x58\x00\x10\x20"
-                + b"\x00\x00\x00\x00\x00\x00\x00\x02"
-                + b"\x00\x00\x21\x34\x00\x00\x07\xd0"
-                + b"\x00\x00\x05\xdc\x00\x00\x05\xdc"
-                + b"\x00\x00\x03\xe8\x00\x00\x00\x01"
-                + b"\x1c\x08")
-    q_status = b"\x01\x03\x00\x7f\x00\x01\xb5\xd2"
-    print("##### QUERY READY")
-    ## MBC確認
-    # while(True):
-    #     client.write(q_mbc)
-    #     time.sleep(0.02)
-    #     response = client.read(size=16)
-    #     print(response)
-    #     time.sleep(0.02)
-    #     if(mbc_status(response) == 0):
-    #         break
-    print("##### INITED")
-    ## 電磁ブレーキOFF
-    client.write(q_release)
-    time.sleep(0.02)
-    response = client.read(size=16)
-    time.sleep(0.02)
-    print("brake off")
-    print(response)
-    time.sleep(1)
-    ## ダイレクトデータ運転
-    client.write(q_direct)
-    time.sleep(0.02)
-    response = client.read(size=16)
-    print("direct data operation")
-    print(response)
-    time.sleep(0.02)
-    time.sleep(1)   # direct data operation now
-    ## MOVE確認
-    while(True):
-        client.write(q_status)
+    # 動作1
+    q_dd1 = (b"\x01\x10\x00\x58\x00\x10\x20\x00\x00\x00\x00\x00\x00\x00\x01"
+             + (1250).to_bytes(4, "big", signed=True)   # 位置
+             + (50000).to_bytes(4, "big")               # 速度
+             + (100000).to_bytes(4, "big")              # 起動・変速レート
+             + (100000).to_bytes(4, "big")              # 停止レート
+             + b"\x00\x00\x03\xe8\x00\x00\x00\x01")
+    q_dd1 += crc_error_check(q_dd1)
+    # 動作2
+    q_dd2 = (b"\x01\x10\x00\x58\x00\x10\x20\x00\x00\x00\x00\x00\x00\x00\x01"
+             + (-1250).to_bytes(4, "big", signed=True)  # 位置
+             + (50000).to_bytes(4, "big")               # 速度
+             + (100000).to_bytes(4, "big")              # 起動・変速レート
+             + (100000).to_bytes(4, "big")              # 停止レート
+             + b"\x00\x00\x03\xe8\x00\x00\x00\x01")
+    q_dd2 += crc_error_check(q_dd2)
+    # リモートI/Oアクセス
+    q_rio = b"\x01\x03\x00\x7f\x00\x01\xb5\xd2"
+
+    # メイン動作
+    for i in range(NUM):
+        ##### 電磁ブレーキを解放する
+        ser.write(q_release)
         time.sleep(0.02)
-        response = client.read(size=16)
+        response = ser.read(16)
         time.sleep(0.02)
-        if(move_status(response) == 0):
-            break
-    ## 電磁ブレーキON
-    client.write(q_keep)
-    time.sleep(0.02)
-    client.read(size=16)
-    time.sleep(0.02)
-    # -------- 終了 --------
-    client.close()
-    print("##### SERIAL CLOSE")
+        print("電磁ブレーキ解放")
 
+        time.sleep(WAIT_ON)
 
-def change_c_on_status(b):
-    client = serial.Serial("/dev/tty.usbserial-FT1GOG9N", 115200, 8, 'E', 1, 0.01)
-    if(b==0):
-        # 解放
-        q = b"\x01\x06\x00\x7d\x00\x04\x18\x11"
-    elif(b==1):
-        # 保持
-        q = b"\x01\x06\x00\x7d\x00\x00\x19\xd2"
-    client.write(q)
-    time.sleep(0.02)
-    response = client.read(size=16)
-    time.sleep(0.02)
-    client.close()
+        ##### モーター動作
+        if(i%2 == 0):
+            ser.write(q_dd1)
+        else:
+            ser.write(q_dd2)
+        time.sleep(0.02)
+        response = ser.read(16)
+        time.sleep(0.02)
+        print("ダイレクトデータクエリ送信")
+        while(True):
+            print("動作待機中")
+            ser.write(q_rio)
+            time.sleep(0.02)
+            response = ser.read(16)
+            if(stop_rotation(ser, response)):
+                break
 
-def make_query():
-    query = b"\x01"
-    # スレーブアドレス
-    ## ドライバにスイッチで割り当てる
-    ## 0はブロードキャスト(*このときクエリ送信してもレスポンスがないので注意)
-    # ファンクションコード
-    ## 使用するのは(読み出し, 書き込み, 複数書き込み)の3つ
-    # データ
-    ## レジスタアドレスや書き込み数（読み出し数）などで長さはNとなる
-    # エラーチェック
-    ## エラーチェック以外のクエリからエラーチェック(CRC-16/Modbus)を算出
-    ## CRC-16/Modbus計算は実装済み
-    return query
+        ##### 電磁ブレーキを保持する
+        ser.write(q_keep)
+        time.sleep(0.02)
+        response = ser.read(16)
+        time.sleep(0.02)
+        print("電磁ブレーキ保持")
 
-def test_main():
-    # シリアル確立
-    client = serial.Serial()
-    set_serial(client)
-    client.open()
-    # ---------------- 開始 ----------------
-    query = make_query()
-    # ---------------- 終了 ----------------
-    client.close()
+        time.sleep(WAIT_OFF)
+    # ---- ---- ---- ----
+    ser.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-    # change_c_on_status(1)
